95a18a5e5c858f0d86f85d881e9eba5f65a76c03
Vihan Patidar <1vihanpatidar6@gmail.com>
2024-08-24 10:52:17 +0530

    done

--- form.js
+++ form.js
@@ -1,43 +1,42 @@
-function validateForm(){
-    const name = document.getElementById("name").value;
-    console.log(name)
-    const nameError = document.getElementById("name-error");
-    nameError.textContent = "";
 
-    let isValid = true;
-    if (name === "" ||/\d/.test(name)) {
-        nameError.textContent =
-            "Please enter your name properly.";
-        isValid = false;
-    }
-    return isValid;
-}
 function validatePhoneNumber() {
-    // Get and trim phone number input
+   
     const phoneNumber = document.getElementById('phonenumber').value.trim();
     console.log(phoneNumber)
 
-    // Regex pattern for 10-digit phone number
     const pattern = /^\d{10}$/;
 
-    // Validate phone number and update message
     const isValid = pattern.test(phoneNumber);
     console.log(isValid)
     document.getElementById('phoneValidationMsg').textContent = isValid ? '' : 'Please enter a valid 10-digit phone number.';
 
-    // Return validation status
     return isValid;
+}
+function validateName(){
+    const fullName = document.getElementById('name').value;
+
+    let pattern =/\d/;
+    const isValid = !pattern.test(fullName)
+    document.getElementById('nameValidationMsg').textContent = isValid?''  : ' Plesase enter a valid name'
+   return isValid
+}
+function validateEmail(){
+    const emailId = document.getElementById("email").value;
+    
 }
 
 document.addEventListener('DOMContentLoaded', function() {
 
-    // Event listener for form submission
     document.getElementById('validationCheck').addEventListener('submit', function(event) {
-
-        // Validate phone number; prevent submission if invalid
+       
         if (!validatePhoneNumber()) {
-            event.preventDefault(); // Stops form submission for invalid number
+            event.preventDefault(); 
         }
+        if (!validateName()){
+            event.preventDefault();
+        }
+        
     });
+
 });
 
